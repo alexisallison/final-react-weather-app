446eee9eb5373d044c589def9889d2975703e6e6
alexisallison <alexisallison1801@gmail.com>
2023-04-22 15:24:38 -0400

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://silver-lokum-739c2e.netlify.app/">Alexis Allison</a>{" "}
-        and is{" "}
-        <a href="https://github.com/alexisallison/final-react-weather-app.git">
-          Open-Sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a href="https://silver-lokum-739c2e.netlify.app/">Alexis Allison</a>{" "}
+          and is{" "}
+          <a href="https://github.com/alexisallison/final-react-weather-app.git">
+            Open-Sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
