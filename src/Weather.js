b3e9a14ea0dd638bc91fa08c92151108df778913
alexisallison <alexisallison1801@gmail.com>
2023-04-22 17:13:24 -0400

    added styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,11 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input type="submit" value="Search" className="btn w-100" />
           </div>
         </div>
       </form>
@@ -23,17 +24,23 @@ export default function Weather() {
         <li>Wednesday 7:00</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          6&deg;C
+          <div className="clearfix d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">&deg;C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitaion: 15%</li>
+            <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
             <li>Wind: 13mph</li>
           </ul>
