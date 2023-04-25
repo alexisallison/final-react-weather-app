27bb9d3d6c6e87f86bb3e091f4cb1172342f5dc0
alexisallison <alexisallison1801@gmail.com>
2023-04-24 21:13:48 -0400

    formatted date

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -14,7 +15,7 @@ export default function Weather(props) {
       city: response.data.city,
       description: response.data.condition.description,
       iconUrl: response.data.condition.icon_url,
-      date: "Saturday 6:30",
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -38,7 +39,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
