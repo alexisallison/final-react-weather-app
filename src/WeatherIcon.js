fb454439d49f766b9812ae5b4696025ef86fee83
alexisallison <alexisallison1801@gmail.com>
2023-05-07 11:35:12 -0400

    weather forecast layout & size prop to icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -19,26 +19,30 @@ import {
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "clear-sky-day": <WiDaySunny size={64} color="#f70776" />,
-    "clear-sky-night": <WiNightClear size={64} color="#f70776" />,
-    "few-clouds-day": <WiDayCloudy size={64} color="#f70776" />,
-    "few-clouds-night": <WiNightAltCloudy size={64} color="#f70776" />,
-    "scattered-clouds-day": <WiDayCloudyWindy size={64} color="#f70776" />,
-    "scattered-clouds-night": (
-      <WiNightAltCloudyWindy size={64} color="#f70776" />
+    "clear-sky-day": <WiDaySunny size={props.size} color="#f70776" />,
+    "clear-sky-night": <WiNightClear size={props.size} color="#f70776" />,
+    "few-clouds-day": <WiDayCloudy size={props.size} color="#f70776" />,
+    "few-clouds-night": <WiNightAltCloudy size={props.size} color="#f70776" />,
+    "scattered-clouds-day": (
+      <WiDayCloudyWindy size={props.size} color="#f70776" />
     ),
-    "broken-clouds-day": <WiCloudy size={64} color="#f70776" />,
-    "broken-clouds-night": <WiCloudy size={64} color="#f70776" />,
-    "shower-rain-day": <WiDayShowers size={64} color="#f70776" />,
-    "shower-rain-night": <WiNightAltShowers size={64} color="#f70776" />,
-    "rain-day": <WiDayRain size={64} color="#f70776" />,
-    "rain-night": <WiNightAltRain size={64} color="#f70776" />,
-    "thunderstorm-day": <WiThunderstorm size={64} color="#f70776" />,
-    "thunderstorm-night": <WiThunderstorm size={64} color="#f70776" />,
-    "snow-day": <WiSnowflakeCold size={64} color="#f70776" />,
-    "snow-night": <WiSnowflakeCold size={64} color="#f70776" />,
-    "mist-day": <WiDayFog size={64} color="#f70776" />,
-    "mist-night": <WiNightFog size={64} color="#f70776" />,
+    "scattered-clouds-night": (
+      <WiNightAltCloudyWindy size={props.size} color="#f70776" />
+    ),
+    "broken-clouds-day": <WiCloudy size={props.size} color="#f70776" />,
+    "broken-clouds-night": <WiCloudy size={props.size} color="#f70776" />,
+    "shower-rain-day": <WiDayShowers size={props.size} color="#f70776" />,
+    "shower-rain-night": (
+      <WiNightAltShowers size={props.size} color="#f70776" />
+    ),
+    "rain-day": <WiDayRain size={props.size} color="#f70776" />,
+    "rain-night": <WiNightAltRain size={props.size} color="#f70776" />,
+    "thunderstorm-day": <WiThunderstorm size={props.size} color="#f70776" />,
+    "thunderstorm-night": <WiThunderstorm size={props.size} color="#f70776" />,
+    "snow-day": <WiSnowflakeCold size={props.size} color="#f70776" />,
+    "snow-night": <WiSnowflakeCold size={props.size} color="#f70776" />,
+    "mist-day": <WiDayFog size={props.size} color="#f70776" />,
+    "mist-night": <WiNightFog size={props.size} color="#f70776" />,
   };
   return codeMapping[props.code];
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix d-flex">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={64} />
 
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
